288b5827299350c58485b4f7643d0587151a166a
jooies <jooies@qq.com>
2021-12-19 00:06:00 +0800

    feat: add StatisticInstance

--- src/statistic/index.ts
+++ src/statistic/index.ts
@@ -5,6 +5,7 @@ export const Statistic = withInstall<typeof _Statistic>(_Statistic)
 
 export default Statistic
 export { statisticProps } from './Statistic'
+export type StatisticInstance = InstanceType<typeof _Statistic> & { update: () => void }
 export type {
     StatisticProps
 } from './Statistic'
