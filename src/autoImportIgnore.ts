44d01777ea4e354fada5f5132da3a9716acd1e01
jooies <jooies@qq.com>
2021-12-21 22:25:40 +0800

    feat: autoImportIgnore

--- src/autoImportIgnore.ts
+++ src/autoImportIgnore.ts
@@ -1,1 +1,19 @@
-export default [ 'close-all-modals', 'modal-props' ]
+export default [
+    'space-props',
+    'grid-props',
+    'grid-item-props',
+    'icon-props',
+    'button-props',
+    'collapse-transition-props',
+    'collapse-props',
+    'collapse-item-props',
+    'popover-props',
+    'tooltip-props',
+    'menu-props',
+    'ellipsis-props',
+    'scroll-list-props',
+    'statistic-props',
+    'overlay-props',
+    'close-all-modals',
+    'modal-props'
+]
