661b31b2b74efd8797df504ece231a43a0dea2d9
jooies <jooies@qq.com>
2022-01-09 16:38:57 +0800

    fix: position

--- src/popover/demo/Placement.vue
+++ src/popover/demo/Placement.vue
@@ -1,6 +1,14 @@
 <template>
-  <o-popover placement="bottom">
-    <template #target> 点我 </template>
-    你好
-  </o-popover>
+    <o-popover placement="bottom-start">
+      <template #target> Bottom Start </template>
+      你好
+    </o-popover>
+    <o-popover placement="bottom">
+      <template #target> Bottom </template>
+      你好
+    </o-popover>
+    <o-popover placement="bottom-end">
+      <template #target> Bottom End </template>
+      你好
+    </o-popover>
 </template>
