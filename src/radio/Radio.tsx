89f7d87ecd987ecaf8cbaa66bd3df6cb1f3a144d
joo1es <76929557+joo1es@users.noreply.github.com>
2022-03-04 10:28:04 +0800

    fix: radio bug

--- src/radio/Radio.tsx
+++ src/radio/Radio.tsx
@@ -63,7 +63,7 @@ export default defineComponent({
             },
             set(value) {
                 if (checkedValue) {
-                    if (!props.value) return
+                    if (props.value === undefined) return
                     checkedValue.value = props.value
                 } else if (props.value !== undefined) {
                     checkedControl.value = props.value
