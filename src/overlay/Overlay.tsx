c10b7f9b446e3f8d8da7e41c08788e5ce50fa261
jooies <jooies@qq.com>
2022-03-26 15:28:54 +0800

    feat: style

--- src/overlay/Overlay.tsx
+++ src/overlay/Overlay.tsx
@@ -14,7 +14,7 @@ export const overlayProps = {
     },
     background: {
         type: String,
-        default: 'rgba(0, 0, 0, .6)'
+        default: 'rgba(0, 0, 0, .75)'
     },
     blur: {
         type: [Boolean, String] as PropType<boolean | string>,
