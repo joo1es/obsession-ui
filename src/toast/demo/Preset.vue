577d029b5189539604641513e933caa44944af52
joo1es <76929557+joo1es@users.noreply.github.com>
2022-03-30 13:53:13 +0800

    feat: add preset

---
+++ src/toast/demo/Preset.vue
@@ -0,0 +1,25 @@
+<template>
+    <o-space>
+        <o-button @click="tip()">
+            成功
+        </o-button>
+        <o-button @click="tip1()">
+            警告
+        </o-button>
+        <o-button @click="tip2()">
+            信息
+        </o-button>
+        <o-button @click="tip3()">
+            错误
+        </o-button>
+    </o-space>
+</template>
+
+<script lang="ts" setup>
+import { h } from 'vue'
+
+const tip = () => Toast.success('我成功了！')
+const tip1 = () => Toast.warning('我被警告了了！')
+const tip2 = () => Toast.info('信息呢？')
+const tip3 = () => Toast.error('出错了！')
+</script>
