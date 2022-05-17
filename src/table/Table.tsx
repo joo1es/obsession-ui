0d3e12efb4b782a871420e037be7562dd7830dde
joo1es <76929557+joo1es@users.noreply.github.com>
2022-05-17 09:17:19 +0800

    feat: add text

--- src/table/Table.tsx
+++ src/table/Table.tsx
@@ -564,12 +564,16 @@ export default defineComponent({
                     case 'selection':
                         return <Checkbox v-model={this.checkAll} indeterminate={this.selections && this.selections.length !== 0} />
                     default:
-                        const Text = (
-                            <span>
-                                {this.$slots[`head-${String(column.prop) || ''}`]?.({ column }) ?? column.label}
-                            </span>
-                        )
-                        const Final: JSX.Element[] = [Text]
+                        const textContent = this.$slots[`head-${String(column.prop) || ''}`]?.({ column }) ?? column.label
+                        const Final: JSX.Element[] = []
+                        if (textContent) {
+                            const Text = (
+                                <span>
+                                    {textContent}
+                                </span>
+                            )
+                            Final.push(Text)
+                        }
                         if (column.sortable) {
                             const SortIcon = (
                                 <Icon
